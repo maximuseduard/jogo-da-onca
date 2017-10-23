2e0c4f382f404a4b12ed4aec0a172b29afa88f50
maximuseduard <31300603+maximuseduard@users.noreply.github.com>
2017-10-23 20:43:44 -0200

    Initial board

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  <meta name="description" content="Trabalho sobre o jogo da onça">
+  <meta name="author" content="André, Eduardo, Ronan">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <meta http-equiv="refresh" content="30"> --> <!-- Refresh document every 30 seconds -->
+  <title>JOGO DA ONÇA</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+  <div id="tab">
+    <span id="x11" class=""> <img src="images/cachorro.png"></span>
+    <span id="x12" class=""> <img src="images/cachorro.png"></span>
+    <span id="x13" class=""> <img src="images/cachorro.png"></span>
+    <span id="x14" class=""> <img src="images/cachorro.png"></span>
+    <span id="x15" class=""> <img src="images/cachorro.png"></span>
+
+    <br/>
+
+    <span id="x21" class=""> <img src="images/cachorro.png"></span>
+    <span id="x22" class=""> <img src="images/cachorro.png"></span>
+    <span id="x23" class=""> <img src="images/cachorro.png"></span>
+    <span id="x24" class=""> <img src="images/cachorro.png"></span>
+    <span id="x25" class=""> <img src="images/cachorro.png"></span>
+
+    <br/>
+
+    <span id="x31" class=""> <img src="images/cachorro.png"></span>
+    <span id="x32" class=""> <img src="images/cachorro.png"></span>
+    <span id="x33" class=""> <img src="images/cachorro.png"></span>
+    <span id="x34" class=""> <img src="images/cachorro.png"></span>
+    <span id="x35" class=""> <img src="images/cachorro.png"></span>
+    <br/>
+
+    <span id="x41" class=""> <img src="images/cachorro.png"></span>
+    <span id="x42" class=""> <img src="images/cachorro.png"></span>
+    <span id="x43" class=""> <img src="images/cachorro.png"></span>
+    <span id="x44" class=""> <img src="images/cachorro.png"></span>
+    <span id="x45" class=""> <img src="images/cachorro.png"></span>
+    <br/>
+
+    <span id="x51" class=""> <img src="images/cachorro.png"></span>
+    <span id="x52" class=""> <img src="images/cachorro.png"></span>
+    <span id="x53" class=""> <img src="images/cachorro.png"></span>
+    <span id="x54" class=""> <img src="images/cachorro.png"></span>
+    <span id="x55" class=""> <img src="images/cachorro.png"></span>
+  </div>
+
+  <br/>
+
+  <span id="x61" class=""> <img src="images/cachorro.png"></span>
+  <span id="x62" class=""> <img src="images/cachorro.png"></span>
+  <span id="x63" class=""> <img src="images/cachorro.png"></span>
+
+  <br/>
+
+  <span id="x71" class=""> <img src="images/cachorro.png"></span>
+  <span id="x72" class=""> <img src="images/cachorro.png"></span>
+  <span id="x73" class=""> <img src="images/cachorro.png"></span>
+
+  <script src="js/jquery-3.2.1.min.js" charset="utf-8"></script>
+</body>
+</html>
